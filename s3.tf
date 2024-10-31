fda731dbf10898606a06da34d430184367196da1
Diego Gelusini <dgelusini@prexcard.com>
2024-10-31 10:59:46 -0300

    terraform nuevo

---
+++ s3.tf
@@ -0,0 +1,14 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
+
+  bucket = var.name-project
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = var.environment == "dev"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,51 @@
+//VARIABLES PARA TAGS 
+//RESPETAR MINUSCULAS, COPIAR Y PEGAR SOLAMENTE
+variable "environment" {
+  description = "Values: [dev]-[prod]-[qa]-[uat]-[pre]-[release]-[stage]-[contingencia]"
+  type        = string
+  default     = "dev"
+}
+variable "workload" {
+  description = "Values: [conectividad]-[monitoreo]-[reporteria]-[aplicativo]-[gestionUsuarios]-[automatismo]-[procesamiento]"
+  type        = string
+  default     = "aplicativo"
+}
+variable "owner" {
+  description = "Values: [infraestructura]-[data]-[security]-[desarrollo]"
+  type        = string
+  default     = "infraestructura"
+}
+variable "entity" {
+  description = "Values: [peru]-[uruguay]-[chile]-[paraguay]-[global]-[houlak]-[procesador]"
+  type        = string
+  default     = "playground"
+}
+variable "backup" {
+  description = "Values: [true]-[false]"
+  type        = string
+  default     = "false"
+}
+variable "tier" {
+  description = "Values: [low]-[medium]-[high]"
+  type        = string
+  default     = ""
+}
+variable "name-project" {
+  description = "Name del proyecto S3 prextamos"
+  type        = string
+  default     = "test-gitactions"
+}
+
+/////////////////////////
+//VARIABLES DEL PROVIDER
+# variable "aws_cuenta" {
+#   description = "Nombre de la cuenta"
+#   type        = string
+#   default     = "prexarrdev"
+# }
+variable "aws_region" {
+  description = "Region de Aws destino"
+  type        = string
+  default     = "us-east-1"
+}
+
