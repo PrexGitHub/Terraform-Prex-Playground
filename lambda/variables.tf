43b71963059a16e13e66d8861a3cf014219ab538
Diego Gelusini <dgelusini@prexcard.com>
2024-12-16 12:04:00 -0300

    nuevo workflow

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -33,7 +33,7 @@ variable "tier" {
 variable "name-project" {
   description = "Name del proyecto S3 prextamos"
   type        = string
-  default     = "test-reguarl-gitactions-prex-playground"
+  default     = "test-regular-gitactions-prex-playground"
 }
 
 /////////////////////////
