ca485cf016d3eb2a003ca5da911e05c2fea1a916
Diego Gelusini <dgelusini@prexcard.com>
2025-01-09 10:59:11 -0300

    Add Terraform module for new infrastructure

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -33,7 +33,7 @@ variable "tier" {
 variable "name-project" {
   description = "Name del proyecto S3 prextamos"
   type        = string
-  default     = "test-gitacregtions-pre-playground"
+  default     = "test-githubcregtions-pre-playground"
 }
 
 /////////////////////////
