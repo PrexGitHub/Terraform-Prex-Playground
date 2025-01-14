0a83157563318eb417a46c727dacdfe88228e660
Diego Gelusini <dgelusini@prexcard.com>
2025-01-14 12:41:56 -0300

    apply

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -33,7 +33,7 @@ variable "tier" {
 variable "name-project" {
   description = "Name del proyecto S3 prextamos"
   type        = string
-  default     = "test-githubcrsegtiosns-pre-playground"
+  default     = "test-githubciosns-pre-playground"
 }
 
 /////////////////////////
