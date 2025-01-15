755215fc5f0fcdc4e0a16503da83605f14ba365b
Gonzalo Erra <gerra@prexcard.com>
2025-01-15 17:33:17 -0300

    Gonzalito

--- lambda/outputs.tf
+++ lambda/outputs.tf
@@ -1,11 +1,11 @@
-output "lambda_function_name" {
-   description = "Nombre de la Lambda function"
-   #value       = module.lambda.function_name
-   value       = module.lambda.lambda_function_name
- }
+# output "lambda_function_name" {
+#    description = "Nombre de la Lambda function"
+#    #value       = module.lambda.function_name
+#    value       = module.lambda.lambda_function_name
+#  }
 
- output "lambda_function_arn" {
-   description = "ARN de la Lambda function"
-   value       = module.lambda.lambda_function_arn
- }
+#  output "lambda_function_arn" {
+#    description = "ARN de la Lambda function"
+#    value       = module.lambda.lambda_function_arn
+#  }
 
